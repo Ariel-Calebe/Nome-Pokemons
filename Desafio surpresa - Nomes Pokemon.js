80f370db0ee101c888a597ce7d22cfdfe17c78ae
Ariel-Calebe <153770940+Ariel-Calebe@users.noreply.github.com>
2024-01-03 19:40:03 -0300

    Update Desafio surpresa - Nomes Pokemon.js

--- Desafio surpresa - Nomes Pokemon.js
+++ Desafio surpresa - Nomes Pokemon.js
@@ -1,20 +1,20 @@
-//  /'''''''''''''''''''''''''''''''\
-// |    Ariel Calebe - 01/01/2024   |
-// \.............................../
+//  /==============================\
+// |   Ariel Calebe - 01/01/2024    |
+//  \==============================/
 //
 // DESAFIO SURPRESA
 // 
-// Crie um código para armazenar o máximo de informações possível dos pokemon da imagem ao lado e ao final, para cada um, 
-// exiba uma mensagem de saída escrita o nome do pokemon concatenado com "Cadastrado com sucesso".
+// Crie um cÃ³digo para armazenar o mÃ¡ximo de informaÃ§Ãµes possÃ­vel dos pokemon da imagem ao lado e ao final, para cada um, 
+// exiba uma mensagem de saÃ­da escrita o nome do pokemon concatenado com "Cadastrado com sucesso".
 
 let pokemon = [
     // Nome      Lvl Sexo  PVAtual PVTotal
     ["Poochyena", 2, "Macho", 13, 13],
-    ["Zigzagoon", 2, "Fêmea", 13, 13],
+    ["Zigzagoon", 2, "FÃªmea", 13, 13],
     ["Dragonite", 5, "Macho", 25, 25],
-    ["Dragonite", 5, "Fêmea", 24, 24],
-    ["Dragonite", 5, "Fêmea", 25, 25],
-    ["Poochyena", 2, "Fêmea", 15, 15],
+    ["Dragonite", 5, "FÃªmea", 24, 24],
+    ["Dragonite", 5, "FÃªmea", 25, 25],
+    ["Poochyena", 2, "FÃªmea", 15, 15],
     ["Wurmple", 2, "Macho", 7, 14]
 ]
 
