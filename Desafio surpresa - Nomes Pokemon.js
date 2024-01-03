7959c3fd9b8420cdbb1d790bb5beb5743106394f
Ariel-Calebe <153770940+Ariel-Calebe@users.noreply.github.com>
2024-01-03 19:38:19 -0300

    Add files via upload

---
+++ Desafio surpresa - Nomes Pokemon.js
@@ -0,0 +1,27 @@
+//  /'''''''''''''''''''''''''''''''\
+// |    Ariel Calebe - 01/01/2024   |
+// \.............................../
+//
+// DESAFIO SURPRESA
+// 
+// Crie um código para armazenar o máximo de informações possível dos pokemon da imagem ao lado e ao final, para cada um, 
+// exiba uma mensagem de saída escrita o nome do pokemon concatenado com "Cadastrado com sucesso".
+
+let pokemon = [
+    // Nome      Lvl Sexo  PVAtual PVTotal
+    ["Poochyena", 2, "Macho", 13, 13],
+    ["Zigzagoon", 2, "Fêmea", 13, 13],
+    ["Dragonite", 5, "Macho", 25, 25],
+    ["Dragonite", 5, "Fêmea", 24, 24],
+    ["Dragonite", 5, "Fêmea", 25, 25],
+    ["Poochyena", 2, "Fêmea", 15, 15],
+    ["Wurmple", 2, "Macho", 7, 14]
+]
+
+console.log(pokemon[0][0] + " - CADASTRADO COM SUCESSO")
+console.log(pokemon[1][0] + " - CADASTRADO COM SUCESSO")
+console.log(pokemon[2][0] + " - CADASTRADO COM SUCESSO")
+console.log(pokemon[3][0] + " - CADASTRADO COM SUCESSO")
+console.log(pokemon[4][0] + " - CADASTRADO COM SUCESSO")
+console.log(pokemon[5][0] + " - CADASTRADO COM SUCESSO")
+console.log(pokemon[6][0] + " - CADASTRADO COM SUCESSO")
